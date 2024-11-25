6a0b17f875ffc278590849bf314f95d6140bd0ab
Aakash Rambhad <146167024+AakashRam70@users.noreply.github.com>
2024-11-26 01:23:36 +0530

    modifying route.ts

--- musicc/app/api/auth/[...nextauth]/route.ts
+++ musicc/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,14 @@
-import { NextResponse } from "next/server";
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
 
-export function GET() {
-    return NextResponse.json({
-        message: "Hello Bro"
-    })
-}
+
+const handler = NextAuth({
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ""
+        })
+    ]
+})
+
+export { handler as GET, handler as POST }
